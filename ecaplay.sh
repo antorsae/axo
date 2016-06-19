2d78d81211d5d88e65f9cd74097b2c5bfbf57b8f
Andres Torrubia <antorsae@gmail.com>
2016-06-19 18:04:58 +0000

    Less latency, 32-bit in

--- ecaplay.sh
+++ ecaplay.sh
@@ -2,12 +2,12 @@
 
 if [ -z "$1" ]
   then
-    PREFIX="/home/pi/dsp/orion-331-matched"
+    PREFIX="/home/pi/axo/orion-331-matched"
   else
     PREFIX="$1"
 fi
-ecasound -d:2  -b:256 -z:mixmode,sum -z:nodb -x -f:s32,2,44100 \
-          -a:pre1 -i:stdin -pf:$PREFIX/pre1.ecp -f:f64 -x -o:loop,1 \
+ecasound -d:2 -z:nodb -b:256 -z:mixmode,sum -x -f:s32,2,44100 \
+          -a:pre1 -i:stdin -pf:$PREFIX/pre1.ecp -f:f64 -o:loop,1 \
           -a:pre2,woofer -i:loop,1 \
           -a:mid,tweeter -i:loop,2 \
           -a:pre2 -pf:$PREFIX/pre2.ecp -o:loop,2 \
